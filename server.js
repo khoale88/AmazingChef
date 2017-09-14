e6008e12b5c6d840b46c016880d537d37ccd642b
khoale88 <lenguyenkhoa1988@gmail.com>
2017-09-13 23:04:54 -0700

    add recipe view

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 app.set("views", __dirname + "/views");
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
 // var cookieParser = require('cookie-parser');
 
 // Create application/x-www-form-urlencoded parser
@@ -14,20 +15,40 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 // app.use(express.static("views"));
 // app.use(cookieParser());
 
+var recipeURL = "/recipe"
+
+// home page
 app.get("/", function(req, res) {
     res.render("home.ejs");
 })
+
+// see each recipe
+app.get("/recipe/:recipe_id", function(req, res) {
+    var recipe = JSON.parse(fs.readFileSync(__dirname + "/recipes/" + req.params["recipe_id"] + ".json", 'utf8'));
+    res.render("recipe", { recipe: recipe });
+})
+
+// search recipes by get
 app.get('/search_recipes', urlencodedParser, function(req, res) {
     var obj = JSON.parse(fs.readFileSync(__dirname + "/recipes/" + "all.json", 'utf8'));
     res.render("search", obj);
 })
 
+// search recipes by post
 app.post('/search_recipes', urlencodedParser, function(req, res) {
-    var obj = JSON.parse(fs.readFileSync(__dirname + "/recipes/" + "all.json", 'utf8'));
-    res.render("search", obj);
+    var omlette = JSON.parse(fs.readFileSync(__dirname + "/recipes/" + "omlette.json", 'utf8'));
+    var potato_curry = JSON.parse(fs.readFileSync(__dirname + "/recipes/" + "potato_curry.json", 'utf8'));
+    var recipes = [];
+    recipes.push(omlette);
+    recipes.push(potato_curry);
+    for (var i = 0; i < recipes.length; i++) {
+        var href = recipeURL + "/" + recipes[i]["recipe_name"].toLowerCase();
+        recipes[i]["href"] = href;
+    }
+    res.render("search", { recipes: recipes });
 })
 
-var server = app.listen(8080, function() {
+var server = app.listen(3000, function() {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
