806f031cc6d515747e30c960cf7c6320cd26c1b6
khoale88 <lenguyenkhoa1988@gmail.com>
2017-09-13 21:34:46 -0700

    add search view

--- server.js
+++ server.js
@@ -2,32 +2,32 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
+
+app.set("views", __dirname + "/views");
+app.set('view engine', 'ejs');
+
 // var cookieParser = require('cookie-parser');
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(express.static("views"));
+// app.use(express.static("views"));
 // app.use(cookieParser());
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/" + "home.html");
-    // res.render("home.html");
+    res.render("home.ejs");
 })
 app.get('/search_recipes', urlencodedParser, function(req, res) {
-    console.log("get");
-    res.sendFile(__dirname + "/recipes/" + "all.json");
+    var obj = JSON.parse(fs.readFileSync(__dirname + "/recipes/" + "all.json", 'utf8'));
+    res.render("search", obj);
 })
 
 app.post('/search_recipes', urlencodedParser, function(req, res) {
-    // console.log("post")
-    res.sendFile(__dirname + "/recipes/" + "all.json");
-    // var obj = JSON.parse(fs.readFileSync(__dirname + "/recipes/" + "all.json", 'utf8'));
-    // res.render("search.html", { data: all })
-    // res.send(obj);
+    var obj = JSON.parse(fs.readFileSync(__dirname + "/recipes/" + "all.json", 'utf8'));
+    res.render("search", obj);
 })
 
-var server = app.listen(8123, function() {
+var server = app.listen(8080, function() {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
